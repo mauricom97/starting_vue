e21be33b0996a670bbc73f76052b58698b1debae
Mauricio <mauricom7@hotmail.com>
2020-08-20 14:16:21 -0300

    camp-email

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h3>Cadastro: </h3>
     <small id="nomeErro" v-show="deuErro">Nenhum dos campos pode ficar em branco!</small><br>
     <input type="text" placeholder="nome" v-model="nomeField"><br>
-    <input type="text" placeholder="email" v-model="emailField"><br>
+    <input type="email" placeholder="email" v-model="emailField"><br>
     <input type="number" placeholder="idade" v-model="idadeField"><br><br>
     <button @click="cadastrarUsuario">Cadastrar</button>
     <hr>
